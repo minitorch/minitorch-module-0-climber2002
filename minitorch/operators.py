f15056ef25842e6e82fa3f37393f19f48127ee14
Kai Wong <user02@tchly.com>
2026-06-21 19:30:46 +1000

    First commit

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -8,6 +8,7 @@ from typing import Callable, Iterable
 #
 # Implementation of a prelude of elementary functions.
 
+
 # Mathematical functions:
 # - mul
 # - id
@@ -33,59 +34,74 @@ from typing import Callable, Iterable
 def mul(x: float, y: float) -> float:
     return x * y
 
+
 def id(x: float) -> float:
     return x
+
 
 def add(x: float, y: float) -> float:
     return x + y
 
+
 def neg(x: float) -> float:
     return -x
+
 
 def lt(x: float, y: float) -> float:
     return 1.0 if x < y else 0.0
 
+
 def eq(x: float, y: float) -> float:
     return 1.0 if x == y else 0.0
+
 
 def max(x: float, y: float) -> float:
     return x if x > y else y
 
+
 def is_close(x: float, y: float) -> bool:
     return abs(x - y) < 1e-2
+
 
 def sigmoid(x: float) -> float:
     if x >= 0:
         return 1.0 / (1.0 + math.exp(-x))
     else:
         return math.exp(x) / (1.0 + math.exp(x))
-    
+
+
 def relu(x: float) -> float:
     return x if x > 0 else 0.0
+
 
 def log(x: float) -> float:
     return math.log(x)
 
+
 def exp(x: float) -> float:
     return math.exp(x)
+
 
 def log_back(x: float, d: float) -> float:
     return d / x
 
+
 def inv(x: float) -> float:
     return 1.0 / x
 
+
 def inv_back(x: float, d: float) -> float:
     return -d / (x * x)
+
 
 def relu_back(x: float, d: float) -> float:
     return d if x > 0 else 0.0
 
 
-
 # ## Task 0.3
 
 # Small practice library of elementary higher-order functions.
+
 
 # Implement the following core functions
 # - map
@@ -100,8 +116,12 @@ def relu_back(x: float, d: float) -> float:
 def map(fn: Callable[[float], float], lst: Iterable[float]) -> Iterable[float]:
     return [fn(x) for x in lst]
 
-def zipWith(fn: Callable[[float, float], float], lst1: Iterable[float], lst2: Iterable[float]) -> Iterable[float]:
+
+def zipWith(
+    fn: Callable[[float, float], float], lst1: Iterable[float], lst2: Iterable[float]
+) -> Iterable[float]:
     return [fn(x, y) for x, y in zip(lst1, lst2)]
+
 
 def reduce(fn: Callable[[float, float], float], lst: Iterable[float]) -> float:
     it = iter(lst)
@@ -110,16 +130,20 @@ def reduce(fn: Callable[[float, float], float], lst: Iterable[float]) -> float:
         result = fn(result, x)
     return result
 
+
 def negList(lst: Iterable[float]) -> Iterable[float]:
     return map(neg, lst)
 
+
 def addLists(lst1: Iterable[float], lst2: Iterable[float]) -> Iterable[float]:
     return zipWith(add, lst1, lst2)
+
 
 def sum(lst: Iterable[float]) -> float:
     if not lst:
         return 0.0
     return reduce(add, lst)
+
 
 def prod(lst: Iterable[float]) -> float:
     return reduce(mul, lst)
